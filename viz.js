a73f0c0a993e5f6725b2e3f4fb5c2dd178cb1ec9
Madison Scott-Clary <madison.scott-clary@canonical.com>
2016-11-27 20:34:55 -0700

    Further work

--- viz.js
+++ viz.js
@@ -1,22 +1,105 @@
 // Grab the file containing all of the responses to the survey
 d3.csv('furry-philosophy-edited.csv', function(data) {
-  // Start an empty variable for holding the text of all responses
-  var questions = '';
+  var aggregate = {};
+
   // Loop through every response to the survey, getting the index
   for (var index in data) {
+    if (index === 'columns') {
+      continue;
+    }
     // Get the response from the data for the given index
     var response = data[index];
     // Loop through every question in the response
     for (var question in response) {
       // Get the answer for the question we're on
       var answer = response[question];
-      // Append the question and answer to our questions variable in the format:
-      // question: answer<newline>
-      questions += question + ": " + answer + '\n';
+      // Make sure that the aggregate already has an entry for the question we
+      // are looking at
+      if (!aggregate[question]) {
+        aggregate[question] = {};
+      }
+      // Try to increment the count for each answer in the aggregate
+      if (aggregate[question][answer]) {
+        aggregate[question][answer]++;
+      } else {
+        aggregate[question][answer] = 1;
+      }
     }
-    // After each response, add a few equals signs so that we can tell responses apart
-    questions += '========\n';
   }
-  // Find the element with class="for-testing" and set its contents to the value of the questions variable
-  d3.select('.for-testing').html(questions);
+  graph(prepData(aggregate));
 });
+
+var philosophyQuestions = [
+  'If we cannot observe something, it may as well not exist.',
+  'There is/are no God, gods, or deities.',
+  'There is no such thing as truth, only opinions.',
+  'Morality is a social construct.',
+  'Sometimes, it is better for individuals to have rights taken away, in order to protect others within a society.',
+  'When in Rome, do as the Romans do.',
+  'People require a national identity to understand themselves.',
+  'The only things we can be said to know are things we have experienced.',
+  'Science is the surest path to knowledge.',
+  'Some races require different treatment than others.',
+  'It is impossible for us to know if other beings are self-aware.',
+  'Without a belief in God, any action becomes permissible.',
+  'It is better to hold false but comforting beliefs, than to know something disturbing.',
+  'The ends can justify the means.',
+  'Everything has a scientific, naturalistic explanation.',
+  'There is an objective reality, which exists independently from us.',
+  'The most important goal of life is to become happy.',
+  'People posses free will, and are able to make choices as individual agents.',
+  'Certain things, such as racism, sexism, and homophobia, are always wrong, regardless of context.',
+  'The media positively contributes to liberal democracy.',
+  'We posses no knowledge at birth.',
+  'Our perceptions accurately represent reality.',
+  'Free market economics is the best way to distribute wealth.',
+  'Different societies ought to keep to themselves.'
+];
+
+function graph(data) {
+  data.forEach(function (question) {
+    nv.addGraph(function() {
+      var chart = nv.models.discreteBarChart()
+          .x(function(d) { return d.label })    //Specify the data accessors.
+          .y(function(d) { return d.value })
+          .staggerLabels(true)    //Too many bars and not enough room? Try staggering labels.
+          .showValues(true)
+          .duration(350);
+
+      var div = d3.select('.vis:nth-of-type(' + question.index + ')');
+      div.select('svg')
+          .datum([question])
+          .call(chart);
+      div.select('h2')
+          .text(question.text)
+
+      nv.utils.windowResize(chart.update);
+
+      return chart;
+    });
+  });
+}
+
+function prepData(data) {
+  var prepped = [];
+  var i = 1;
+  philosophyQuestions.forEach(function(question) {
+    prepped.push({
+      key: question,
+      index: i++,
+      values: prepQuestion(data[question])
+    })
+  });
+  return prepped;
+}
+
+function prepQuestion(question) {
+  var prepped = [];
+  for (var response in question) {
+    prepped.push({
+      'label': response,
+      'value': question[response],
+    })
+  }
+  return prepped;
+}
